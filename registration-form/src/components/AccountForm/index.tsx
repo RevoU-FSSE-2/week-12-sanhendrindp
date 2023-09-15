1a935a185ee18c76b3639670256ce08bcd3e65ab
Sanhendrin Dwi Prasetya <sanhendrin.dwip@gmail.com>
2023-09-16 00:21:36 +0700

    feat: :sparkles: Use formik and yup as validation on AddressForm & AccountForm

--- registration-form/src/components/AccountForm/index.tsx
+++ registration-form/src/components/AccountForm/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { Form, Input, Button } from "antd";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Input, Button } from "antd";
+import * as Yup from "yup";
+import "./AccountForm.css";
 
 interface AccountFormProps {
   onFinish: (values: AccountFormData) => void;
@@ -15,31 +18,68 @@ const AccountForm: React.FC<AccountFormProps> = ({
   onFinish,
   initialValues,
 }) => {
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().required("Please enter your username"),
+    password: Yup.string()
+      .required("Please enter your password")
+      .matches(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+        "Use at least 8 characters, one uppercase letter, one lowercase letter, and one number"
+      ),
+  });
+
   return (
-    <Form<AccountFormData> onFinish={onFinish} initialValues={initialValues}>
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[{ required: true, message: "Please enter your username" }]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Password"
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: "Please enter your password",
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-      <Button type="primary" htmlType="submit">
-        Continue
-      </Button>
-    </Form>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        onFinish(values);
+      }}
+    >
+      {() => (
+        <Form>
+          <div className="form-group">
+            <label htmlFor="username">
+              <span className="star">*</span> Username
+            </label>
+            <Field
+              as={Input}
+              type="text"
+              name="username"
+              id="username"
+              className="form-control"
+            />
+            <ErrorMessage
+              name="username"
+              component="div"
+              className="error-message"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">
+              <span className="star">*</span> Password
+            </label>
+            <Field
+              as={Input.Password}
+              type="password"
+              name="password"
+              id="password"
+              className="form-control"
+            />
+            <ErrorMessage
+              name="password"
+              component="div"
+              className="error-message"
+            />
+          </div>
+
+          <Button type="primary" htmlType="submit">
+            Continue
+          </Button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
--- registration-form/src/components/AddressForm/index.tsx
+++ registration-form/src/components/AddressForm/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
-import { Form, Input, Button } from "antd";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Input, Button } from "antd";
+import * as Yup from "yup";
+import "./AddressForm.css";
 
 interface AddressFormProps {
   onFinish: (values: AddressFormData) => void;
@@ -17,57 +20,101 @@ const AddressForm: React.FC<AddressFormProps> = ({
   onFinish,
   initialValues,
 }) => {
+  const validationSchema = Yup.object().shape({
+    streetAddress: Yup.string().required("Please enter your street address"),
+    city: Yup.string().required("Please enter your city"),
+    state: Yup.string().required("Please enter your state"),
+    zipCode: Yup.string().required("Please enter your zip code"),
+  });
+
   return (
-    <Form<AddressFormData> onFinish={onFinish} initialValues={initialValues}>
-      <Form.Item
-        label="Street Address"
-        name="streetAddress"
-        rules={[
-          { required: true, message: "Please enter your street address" },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="City"
-        name="city"
-        rules={[
-          {
-            required: true,
-            message: "Please enter your city",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="State"
-        name="state"
-        rules={[
-          {
-            required: true,
-            message: "Please enter your state",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        label="Zip Code"
-        name="zipCode"
-        rules={[
-          {
-            required: true,
-            message: "Please enter your zip code",
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Button type="primary" htmlType="submit">
-        Continue
-      </Button>
-    </Form>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(values) => {
+        onFinish(values);
+      }}
+    >
+      {() => (
+        <Form>
+          <div className="form-group">
+            <label htmlFor="streetAddress">
+              <span className="star">*</span> Street Address
+            </label>
+            <Field
+              as={Input}
+              type="text"
+              name="streetAddress"
+              id="streetAddress"
+              className="form-control"
+            />
+            <ErrorMessage
+              name="streetAddress"
+              component="div"
+              className="error-message"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="city">
+              <span className="star">*</span> City
+            </label>
+            <Field
+              as={Input}
+              type="text"
+              name="city"
+              id="city"
+              className="form-control"
+            />
+            <ErrorMessage
+              name="city"
+              component="div"
+              className="error-message"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="state">
+              <span className="star">*</span> State
+            </label>
+            <Field
+              as={Input}
+              type="text"
+              name="state"
+              id="state"
+              className="form-control"
+            />
+            <ErrorMessage
+              name="state"
+              component="div"
+              className="error-message"
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="zipCode">
+              <span className="star">*</span> Zip Code
+            </label>
+            <Field
+              as={Input}
+              type="text"
+              name="zipCode"
+              id="zipCode"
+              className="form-control"
+            />
+            <ErrorMessage
+              name="zipCode"
+              component="div"
+              className="error-message"
+            />
+          </div>
+
+          <Button type="primary" htmlType="submit">
+            Continue
+          </Button>
+        </Form>
+      )}
+    </Formik>
   );
 };
 
